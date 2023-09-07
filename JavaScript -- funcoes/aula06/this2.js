049745c376913d825da4e3f96700f68be3fb762f
Daniel Vasconcelos <57712464+Daniel-sena-dev@users.noreply.github.com>
2023-09-07 19:25:51 -0300

    estudos this/bind/obj/for

---
+++ JavaScript -- funcoes/aula06/this2.js
@@ -0,0 +1,34 @@
+pessoa = {
+    nome: 'Daniel',
+    idade: '24',
+    curso: 'CeT',
+    //METODO DO OBJETO
+    saudacao(){
+        console.log(`Olá ${this.nome}`)
+    }
+}
+
+//pessoa.nome()
+const func = pessoa.saudacao.bind(pessoa)
+func()
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.curso)
+
+console.log('--------')
+//Utilizando for in pra imprimir valores de um objeto
+for(i in pessoa){
+    console.log(pessoa[i])
+}
+
+//-------------
+console.log('--------')
+let vetor = []
+for(i = 5; i > 0; i--){
+    vetor[i] = i
+}
+console.log('Imprimir os valores do vetor criado pelo "for" com o "for in"')
+for(i in vetor){
+    console.log(vetor[i])
+}
+
