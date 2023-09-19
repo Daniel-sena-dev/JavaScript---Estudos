01294e35f48a05421bf29c57a86b97b90cf950e2
Daniel Vasconcelos <57712464+Daniel-sena-dev@users.noreply.github.com>
2023-09-19 16:46:38 -0300

    Estudos 19/09 - JS

--- JavaScript/aula09/main.js
+++ JavaScript/aula09/main.js
@@ -89,10 +89,78 @@ function verificarPrimo(){
 
     if(primo == 2){
         saida.innerHTML = `<h2>O número ${verificar} é primo</h2>`
+    } else{
+        saida.innerHTML = `<h2>O número ${verificar} não é primo</h2>`
     }
 
     //Limpar input
     document.getElementById('iNumeroPrimo').value = ''
     document.getElementById('iNumeroPrimo').focus()
-
 }
+
+function gerarEstrelas(){
+    let saida = saidaTela()
+    let iQuantidade = document.getElementById('iQuantidade')
+    let quantidade = Number(iQuantidade.value)
+    
+    for(i = 1; i <= quantidade; i++){
+        if(i % 2 == 0){
+            saida.innerHTML += `_`
+        } else{
+            saida.innerHTML += `*`
+        }
+    }
+
+    //Limpar input
+    iQuantidade.value = ''
+    iQuantidade.focus()
+}
+
+function repeteFruta(){
+    let saida = saidaTela()
+    let iFruta = document.getElementById('iFruta')
+    let fruta = iFruta.value
+    let iQuantidade = document.getElementById('iQuantidade')
+    let quantidade = Number(iQuantidade.value)
+
+    for(i=0; i < quantidade; i++){
+        if(i != quantidade - 1){     
+            saida.innerHTML += `${fruta} * `
+        } else{
+            saida.innerHTML += `${fruta}`
+        }
+    }
+
+    //Limpar inputs
+    iFruta.value = ''
+    iQuantidade.value = ''
+    iFruta.focus()
+}
+
+function criarChinchilas(){
+    let saida = saidaTela()
+    let iQuantidade = document.getElementById('iQuantidade')
+    let quantidade = Number(iQuantidade.value)
+    let iAnoQuantidade = document.getElementById('iAnoQuantidade')
+    let anoQuantidade = Number(iAnoQuantidade.value)
+    saida.innerHTML = ``
+
+    if(quantidade < 2){
+        saida.innerHTML = `<p>Quantidade de Chinchilas insuficiente! Digite ao menos 2</p>`
+        saida.style.color = 'red'
+    } else{
+        saida.innerHTML += `<p>1º Ano: ${quantidade} Chinchilas</p>`
+        for(i=1; i < anoQuantidade; i++){
+            saida.innerHTML += `<p>${i + 1}º Ano: ${quantidade * 3} Chinchilas</p>`
+            quantidade *= 3
+        }
+        
+    }
+
+    //Limpando inputs
+    iQuantidade.value = ''
+    iAnoQuantidade.value = ''
+    iQuantidade.focus()
+
+
+}   
